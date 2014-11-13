607d6a3477cb356d9c2ecfde4e23b92f240da4fe
Ignacio Juan Martín Benedetti <tranceway@gmail.com>
2014-11-13 15:34:23 -0300

    Activación de Team y Activities

--- cuaderno/cuaderno/settings.py
+++ cuaderno/cuaderno/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # own apps
+    'team',
+    'activities',
 )
 
 MIDDLEWARE_CLASSES = (
