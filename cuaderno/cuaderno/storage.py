dd6a8808959c04757fe02990168c0eebb463f0d9
Ignacio Juan Martín Benedetti <tranceway@gmail.com>
2014-12-01 14:59:37 -0300

    Implementación de métodos de Storage

--- cuaderno/cuaderno/storage.py
+++ cuaderno/cuaderno/storage.py
@@ -28,3 +28,31 @@ class OwnCloudStorage(Storage):
 
         except ResponseError:
             return False
+
+    def delete(self, name):
+        try:
+            self.oc.delete(name)
+            return True
+
+        except ResponseError:
+            return False
+
+    def listdir(self, path):
+        directories, files = [], []
+
+        for e in self.oc.list(path):
+            if e.file_type == 'dir':
+                directories.append(os.path.join(path, e.name))
+            else:
+                files.append(os.path.join(path, e.name))
+
+        return directories, files
+
+
+    def size(self, name):
+        data = self.oc.file_info()
+        return data.get_size()
+
+    def url(self, name):
+        data = self.oc.share_file_with_link(name)
+        return '%s%s' % (data.link, '&download')
