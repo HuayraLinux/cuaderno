906d63464f26538277827da65ca3ae14cd6839f9
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-12-22 16:59:44 -0300

    Creando la vista principal y detalle.

--- cuaderno/activities/views.py
+++ cuaderno/activities/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render_to_response
+from activities.models import Activity
 
-# Create your views here.
+def index(request):
+    q = request.GET.get('q', None)
+    activities = Activity.objects.all()
+
+    if q:
+        activities = activities.filter(name__icontains=q)
+
+    return render_to_response('activities/index.html', {'activities': activities})
+
+def show(request, id):
+    try:
+        activity = Activity.objects.get(pk=id)
+    except Activity.DoesNotExist:
+        raise Http404
+
+    return render_to_response('activities/show.html', {'activity': activity})
--- cuaderno/cuaderno/urls.py
+++ cuaderno/cuaderno/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^$', 'activities.views.index', name='index'),
+    url(r'^(?P<id>\d+)', 'activities.views.show', name='show')
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
